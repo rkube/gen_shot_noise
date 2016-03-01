620aaf1ae9cbe6e3cd138d6c8c2f9e91a0db8d47
rkube <ralph_kube@gmx.net>
2016-03-01 16:24:41 +0100

    Added attributes for most valus in config. Missing: vector attributes(?) for distribution parameters

--- output.cpp
+++ output.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include "include/gen_shot_noise.h"
 #include "include/output.h"
 
 using namespace std;
@@ -18,14 +21,67 @@ output_t :: output_t(config_t config_) : sn_config(config_),
 
     // Create dataset and write to file
     dataset = new H5::DataSet(file.createDataSet("shotnoise", H5::PredType::NATIVE_DOUBLE, file_space, prop_list));
-    cout << "done" << endl;
+
+    // Write the configuration file as attributes
+    map<dist_t, std::string> my_map{
+        {dist_t::expon_t, string("exponential")},
+        {dist_t::normal_t, string("normal")},
+        {dist_t::uniform_t, string("uniform")}};
+
+    int att_val_int{0};
+    double att_val_double{0.0};
+    H5std_string att_str;
+
+    // Create new string datatype for attribute
+    H5::StrType strdatatype(H5::PredType::C_S1, 32); // of length 256 characters
+
+    H5::DataSpace attrib_space(H5S_SCALAR);
+    H5::Attribute att = dataset -> createAttribute("num_bursts", H5::PredType::NATIVE_INT, attrib_space);
+    att_val_int = sn_config.get_num_bursts();
+    att.write(H5::PredType::NATIVE_INT, &att_val_int);
+    
+    att = dataset -> createAttribute("dt", H5::PredType::NATIVE_DOUBLE, attrib_space);
+    att_val_double = sn_config.get_dt();
+    att.write(H5::PredType::NATIVE_DOUBLE, &att_val_double);
+
+    att = dataset -> createAttribute("Tend", H5::PredType::NATIVE_DOUBLE, attrib_space);
+    att_val_double = sn_config.get_Tend();
+    att.write(H5::PredType::NATIVE_DOUBLE, &att_val_double);
+
+    att = dataset -> createAttribute("Lpar", H5::PredType::NATIVE_DOUBLE, attrib_space);
+    att_val_double = sn_config.get_Lpar();
+    att.write(H5::PredType::NATIVE_DOUBLE, &att_val_double);
+
+    att = dataset -> createAttribute("Cs", H5::PredType::NATIVE_DOUBLE, attrib_space);
+    att_val_double = sn_config.get_Cs();
+    att.write(H5::PredType::NATIVE_DOUBLE, &att_val_double);
+
+    att = dataset -> createAttribute("Lsol", H5::PredType::NATIVE_DOUBLE, attrib_space);
+    att_val_double = sn_config.get_Lsol();
+    att.write(H5::PredType::NATIVE_DOUBLE, &att_val_double);
+
+    att = dataset -> createAttribute("Amplitude distribution", strdatatype, attrib_space);
+    att_str = my_map.at(sn_config.get_dist_amp_type());
+    att.write(strdatatype, att_str);
+
+    att = dataset -> createAttribute("Arrival time distribution", strdatatype, attrib_space);
+    att_str = my_map.at(sn_config.get_dist_arrival_type());
+    att.write(strdatatype, att_str);
+
+    att = dataset -> createAttribute("Length distribution", strdatatype, attrib_space);
+    att_str = my_map.at(sn_config.get_dist_length_type());
+    att.write(strdatatype, att_str);
+
+    att = dataset -> createAttribute("Velocity distribution", strdatatype, attrib_space);
+    att_str = my_map.at(sn_config.get_dist_vrad_type());
+    att.write(strdatatype, att_str);
+
+
 }
 
 
 void output_t :: write_to_tlev(size_t xi_idx, double* data)
 {
-    cout << "writing for xi =  " << xi_idx << endl;
-
     // We need a reference to this guy later on
     const hsize_t nelem{sn_config.get_nelem()};
     //Select hyperslab in the dataset of the output file 
